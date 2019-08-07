f7ec3b3aa97ffbeb61a8c4c3703ffbbb66cb94cd
vlntnpwls <53047232+vlntnpwls@users.noreply.github.com>
2019-08-07 15:47:22 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+<style scoped lang="scss">
+</style>
