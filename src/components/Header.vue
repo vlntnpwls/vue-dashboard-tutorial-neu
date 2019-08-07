98ee43c3ced2fa4af1bd5ce7a27948d4e0c6cac2
vlntnpwls <53047232+vlntnpwls@users.noreply.github.com>
2019-08-07 18:26:14 +0200

    After Login Setup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,34 @@
   <div class="home">
     <div id="nav">
       <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <a @click="onClick">Logout</a>
     </div>
   </div>
 </template>
 
 <script>
+import { auth } from "@/main";
+
 export default {
-  name: "Header"
+  name: "Header",
+  methods: {
+    onClick() {
+      const user = auth.currentUser();
+
+      user
+        .logout()
+        .then(response => {
+          this.$router.push({
+            name: "signin",
+            params: { userLoggedOut: true }
+          });
+        })
+        .catch(error => {
+          alert("Error:", error);
+        });
+    }
+  }
 };
 </script>
 <style scoped lang="scss">
