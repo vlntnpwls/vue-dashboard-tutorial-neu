6731ec9cb1fe49ad12dc35ee4f89b06ed12dadc9
vlntnpwls <53047232+vlntnpwls@users.noreply.github.com>
2019-08-15 15:49:51 +0200

    after slack api

--- src/views/SignInFlow/Request.vue
+++ src/views/SignInFlow/Request.vue
@@ -1,12 +1,109 @@
 <template>
-  <div></div>
+  <div
+    class="container"
+    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+  >
+    <RequestAccount />
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode" />
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode" />
+      <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Request Account</h4>
+      <form @submit.prevent="onSubmit">
+        <input
+          type="email"
+          placeholder="Email"
+          :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+          v-model="email"
+          required
+        />
+        <button>Request Account</button>
+      </form>
+      <router-link
+        to="/signin"
+        :class="{'light-link': isDarkMode, 'dark-link' : !isDarkMode}"
+      >Already have an account? Sign in now.</router-link>
+      <ThemeSwitch />
+    </div>
+  </div>
 </template>
 
 <script>
+import ThemeSwitch from "@/components/ThemeSwitch";
+
 export default {
-  name: "Request"
+  name: "Request",
+  components: {
+    ThemeSwitch
+  },
+  data() {
+    return {
+      email: null,
+      password: null,
+      hasText: false,
+      text: ""
+    };
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  },
+  methods: {
+    onSubmit() {
+      const email = this.email;
+
+      // Slack API logic
+      let slackURL = new URL("https://slack.com/api/chat.postMessage");
+
+      const data = {
+        token:
+          "xoxp-716052431379-727493677232-727033249204-c71c3701759751a01f37b4e587c7548f",
+        channel: "hq",
+        text: `${email} has requested admin access to HQ. Please go to Netlify to invite them.`
+      };
+
+      slackURL.search = new URLSearchParams(data);
+
+      fetch(slackURL)
+        .then(() => {
+          this.$router.push({
+            name: "signin",
+            params: {
+              userRequestedAccount: true,
+              email: email
+            }
+          });
+        })
+        .catch(error => {
+          alert("Error: " + error);
+        });
+    }
+  },
+  mounted() {
+    const params = this.$route.params;
+
+    if (params.userLoggedOut) {
+      this.hasText = true;
+      this.text = "You have logged out!";
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding-top: 16px;
+  padding-bottom: 24px;
+}
+
+.login {
+  width: 400px;
+  text-align: center;
+  margin-left: 16px;
+  margin-right: 16px;
+}
 </style>
